ce3ac9a0e42c1215fb1cd70106f7c6fb69c8d0e5
Jurda5 <jan.jurica.v@gmail.com>
2016-11-14 23:51:58 +0100

    Simulator a Posadka done

---
+++ page-2.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+	<style>
+        .article{
+            background: #fff;
+            border-top: 1px solid rgba(51, 51, 51, 0.1);
+            padding: 2%;
+            margin-left: 1%;
+            margin-right: 1%;
+        }
+        
+        .member div{
+            float: left;
+        }
+        .member-text{
+            width: 70%;
+            padding-right: 2%;
+            margin-top: 4%;
+            text-align: justify;
+        }
+        .member-pic{
+            width: 30%;
+            padding-right: 2%;
+            padding-top: 4%;
+            text-align: center;
+        }
+        
+        .member img{
+            border-radius: 200px;
+            max-width: 95%;
+        }
+        
+        @media screen and (max-width: 60em) {
+            
+        }
+        @media screen and (max-width: 44em) {
+            .member div{
+                float: none;
+            }
+            .member h2{
+                text-align: center;
+            }
+            
+            .member-text{
+                width: 100%;
+            }
+            .member-pic{
+                width: 100%;
+            }
+            .member-pic img{
+                max-width: 40%;
+            }
+        }
+	</style>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<article class="article">
+                <div class="hlavni-pos">
+                <h1>Hlavní posádka roku 2017</h1>
+                    <div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="zalozni-pos">
+                <h1>Záložní posádka roku 2017</h1>
+                    <div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div><div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div><div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div><div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div><div class="member">
+                        <div class="member-pic">
+                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
+                        <div class="member-text">
+                        <h2>Samuel Matějíčka</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                        </div>
+                    </div>
+                </div>
+                <h1></h1>
+			</article>
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
---
+++ page-4.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+get_header(); ?>
+	<style>
+        .article{
+            background: #fff;
+            border-top: 1px solid rgba(51, 51, 51, 0.1);
+            padding: 2%;
+            margin-left: 1%;
+            padding-right: 0;
+        }
+        .text-part{
+            z-index: 0;
+            width: 75%;
+            position: relative;
+            text-align: justify;
+        }
+        .pic-part{
+            position:absolute;
+        }
+        .pic-res{
+            display: none;
+        }
+        .text-part .odstavec1{
+            margin-top: 9%;
+        }
+        .text-part .odstavec2{
+            margin-top: 12%;
+        }
+        .text-part .odstavec3{
+            margin-top: 20%;
+        }
+        .text-part .odstavec4{
+            margin-top: 5%;
+        }
+        @media screen and (max-width: 60em) {
+            .text-part{
+                width: 70%;
+            }
+        }
+        @media screen and (max-width: 44em) {
+            .pic-part{
+                width: 100%;
+                position: relative;
+            }
+            .text-part{
+                width: 100%;
+                margin-right: 2%;
+            }
+            .text-part .odst{
+                margin-top: auto;
+            }
+            .text-part h2{
+                text-align: center;
+            }
+            .pic-part .pic-norm{
+                display: none;
+            }
+            .pic-res{
+                display: block;
+            }
+        }
+	</style>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<article class="article">
+                <h1>Simulátor</h1>
+                <div class="pic-part">
+                    <img src="<?=get_bloginfo('template_directory');?>/img/Ship.png" style="width:100%;" class="pic-norm">
+                    <img src="<?=get_bloginfo('template_directory');?>/img/ShipSmall.png" style="width:100%;" class="pic-res">
+                </div>
+                <div class="text-part">
+                    <div class="odstavec1 odst">
+                    <h2>Hlavní modul</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                    </div>
+                    <div class="odstavec2 odst">
+                    <h2>Laboratoř</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                    </div>
+                    <div class="odstavec3 odst">
+                    <h2>Servisní modul (záchod)</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                    </div>
+                    <div class="odstavec4 odst">
+                    <h2>Přistávací modul</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
+                    </div>
+                </div>
+			</article>
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
