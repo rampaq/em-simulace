a2e436997781b98286c32aed7c46d462c3da8a22
rampaq <tomasfaikl@seznam.cz>
2016-11-19 17:19:16 +0100

    Rozdělení hlavní stránky, menší úprava stylů, přerozdělení id

--- page-1.php
+++ page-1.php
@@ -17,12 +17,12 @@ get_header(); ?>
 			width: 90%;
 			margin: 3.5% 5%;
 			background-color: #FFF;
-			font-size: .8em;
+			font-size: 1em;
 		}
 		.full th {
 			text-align: center;
 			font-family: 'Righteous';
-			font-size: 3em;
+			font-size: 2em;
 		}
 		.full td {
 			width: 100%;
@@ -30,64 +30,26 @@ get_header(); ?>
 			text-align: justify;
 		}
 
-		.w_img {
-			text-align: justify;
-			width: 100%;
-			margin: 3.5% 0;
-			background-color: #FFF;
-			font-size: .8em;
-		}
-		.w_img th {
-			width: 60%;
-			text-align: center;
-			font-family: 'Righteous';
-			font-size: 2.4em;
+		article > h1 {
+			margin: 0 10%;
 		}
 
-		.w_img td {
-			width: 60%;
-			padding: 2em;
-			text-align: justify;
-		}
-		.w_img .img{
-			width: 40%;
-		}
-
-		.participants {
-			position: relative;
-			text-align: center;
-			width: 100%;
-			height: 675px;
-		}
-
-		.medallion {
-			display: inline-block;
-			text-align: center;
-			width: 260px;
-			position: absolute;
-			background-color: rgba(255, 150, 30, .2);
-			border: solid 1px rgb(255,150,30);
-			padding: 1em;
-		}
-		.medallion img {
-			width: 200px;
-		}
 	</style>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<article class="hentry" style="float: left;">
-				<div class=header_cont style="width: 100%; height: 400px; float: left; margin-top: -9%;margin-bottom: 2em;">
+				<div class=header_cont style="width: 100%; height: 200px; float: left; margin-top: -9%;margin-bottom: 2em;">
 					<div class="bg" style="background: #d25302 url(<?=get_bloginfo('template_directory');?>/img/quality_header.png) no-repeat 50% 50%; background-size: cover; height: 100%;">
 					</div>
 				</div>
 				<table class="full">
+				<h1>Simulace Expedice Mars</h1>
 					<tr>
-						<th>O simulaci</th>
+						<th>Základní informace</th>
 					</tr>
 					<tr>
 						<td>
-							<a href="http://expedicemars.eu"><img style="float: right; width: 15em; margin: -1em 1em 1em" src="<?=get_bloginfo('template_directory');?>/img/em.jpg" alt="logo Expedice Mars"></a>
 							<p>Where ignorance lurks, so too do the frontiers of discovery and imagination.</p>
 							<p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
 
@@ -96,88 +58,6 @@ get_header(); ?>
 					</tr>
 				</table>
 
-				<hr>
-				<span>Nějaký grafický prvek.</span>
-				<hr>
-
-				<table class="w_img">
-				<tr>
-					<td rowspan="2" class="img">Společná fotka.</td>
-					<th>Kdo jsme</th>
-				</tr>
-				<tr>
-					<td>
-						<p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
-					</td>
-					</tr>
-				</table>
-
-				<table class="w_img">
-				<tr>
-					<th>Vychloubání</th>
-					<td rowspan="2" class="img">Fotka ze simulace.</td>
-				</tr>
-				<tr>
-					<td>
-						<p>Where ignorance lurks, so too do the frontiers of discovery and imagination.</p>
-						<p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
-					</td>
-					</tr>
-				</table>
-
-				<table class="full">
-					<tr>
-						<th>Posádka roku 2017</th>
-					</tr>
-					<tr>
-						<td>
-							<p>Where ignorance lurks, so too do the frontiers of discovery and imagination.</p>
-							<p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
-							<p>Jména, ...</p>
-							<div class="participants">
-								<div class="medallion" style="left:calc(30% - 130px);">
-									<img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt="">
-									<h2>Samuel Matějíčka</h2>
-									<span>konstruktér, SK</span>
-								</div>
-								<div class="medallion" style="right:calc(30% - 130px);">
-									<img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt="">
-									<h2>Tomáš Faikl</h2>
-									<span>inženýr, CZ</span>
-								</div>
-								<div class="medallion" style="top:400px;left:calc(15% - 130px);">
-									<img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt="">
-									<h2>Jakub Janoušek</h2>
-									<span>astronom, CZ</span>
-								</div>
-								<div class="medallion" style="top:400px;left:calc(50% - 130px)">
-									<img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt="">
-									<h2>Natalia Wirthová</h2>
-									<span>lékařka, SK</span>
-								</div>
-								<div class="medallion" style="top:400px;right:calc(15% - 130px);">
-									<img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt="">
-									<h2>Beáta Plaskurová</h2>
-									<span>astronomka, SK</span>
-								</div>
-							</div>
-						</td>
-					</tr>
-				</table>
-
-				<table class="w_img">
-				<tr>
-					<td rowspan="2" class="img">
-						Mapa.
-					</td>
-					<th>Místo</th>
-				</tr>
-				<tr>
-					<td>
-						<p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
-					</td>
-				</tr>
-				</table>
 			</article>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
--- page-2.php
+++ page-2.php
@@ -13,144 +13,122 @@
 
 get_header(); ?>
 	<style>
-        .article{
-            background: #fff;
-            border-top: 1px solid rgba(51, 51, 51, 0.1);
-            padding: 2%;
-            margin-left: 1%;
-            margin-right: 1%;
-        }
-        
-        .member div{
-            float: left;
-        }
-        .member-text{
-            width: 70%;
-            padding-right: 2%;
-            margin-top: 4%;
-            text-align: justify;
-        }
-        .member-pic{
-            width: 30%;
-            padding-right: 2%;
-            padding-top: 4%;
-            text-align: center;
-        }
-        
-        .member img{
-            border-radius: 200px;
-            max-width: 95%;
-        }
-        
-        @media screen and (max-width: 60em) {
-            
-        }
-        @media screen and (max-width: 44em) {
-            .member div{
-                float: none;
-            }
-            .member h2{
-                text-align: center;
-            }
-            
-            .member-text{
-                width: 100%;
-            }
-            .member-pic{
-                width: 100%;
-            }
-            .member-pic img{
-                max-width: 40%;
-            }
-        }
+		td {
+			height: 100%;
+		}
+		#srm_gmp_embed_1 {
+			height: 100%;
+			width: 100%;
+			position: absolute;
+			top:0;
+			left: 0;
+			margin: 0 !important;
+		}
+		.full {
+			width: 90%;
+			margin: 3.5% 5%;
+			background-color: #FFF;
+			font-size: 1em;
+		}
+		.full th {
+			text-align: center;
+			font-family: 'Righteous';
+			font-size: 2em;
+		}
+		.full td {
+			width: 100%;
+			padding: 2em;
+			text-align: justify;
+		}
+
+		.w_img {
+			text-align: justify;
+			width: 90%;
+			margin: 3.5% 5%;
+			background-color: #FFF;
+			font-size: 1em;
+		}
+		.w_img th {
+			width: 50%;
+			text-align: center;
+			font-family: 'Righteous';
+			font-size: 2em;
+		}
+
+		.w_img td {
+			width: 50%;
+			padding: 2em;
+			text-align: justify;
+		}
+		.w_img .img{
+			width: 50%;
+			background-color: #fff;
+			background-position: center center;
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+
+		article > h1 {
+			margin: 0 10%;
+		}
+
 	</style>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<article class="article">
-                <div class="hlavni-pos">
-                <h1>Hlavní posádka roku 2017</h1>
-                    <div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div>
-                    <div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div>
-                    <div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div>
-                    <div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div>
-                    <div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="zalozni-pos">
-                <h1>Záložní posádka roku 2017</h1>
-                    <div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div><div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div><div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div><div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div><div class="member">
-                        <div class="member-pic">
-                        <img src="<?=get_bloginfo('template_directory');?>/img/participant.jpg" alt=""></div>
-                        <div class="member-text">
-                        <h2>Samuel Matějíčka</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit nisi elementum tincidunt convallis. Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.Vivamus consequat turpis blandit eleifend congue. Aenean sodales tincidunt dictum.</p>
-                        </div>
-                    </div>
-                </div>
-                <h1></h1>
+			<article class="hentry" style="float: left;">
+				<h1>O projektu</h1>
+				<table class="w_img">
+					<tr>
+						<th>Kdo jsme</th>
+						<td rowspan="2" class="img" style="background-image: url('<?=get_bloginfo('template_directory');?>/img/unsplash1.jpg');"></td>
+
+					</tr>
+					<tr>
+						<td>
+							<p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
+						</td>
+					</tr>
+				</table>
+
+				<table class="w_img">
+				<tr>
+					<td rowspan="2" class="img" style="background-image: url('<?=get_bloginfo('template_directory');?>/img/unsplash2.jpg');"></td>
+					<th>Vychloubání</th>
+				</tr>
+				<tr>
+					<td>
+						<p>Where ignorance lurks, so too do the frontiers of discovery and imagination.</p>
+						<p>As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm, living object looked so fragile, so delicate, that if you touched it with a finger it would crumble and fall apart. Seeing this has to change a man.</p>
+					</td>
+					</tr>
+				</table>
+
+				<table class="full">
+					<tr>
+						<th>Posádka roku 2017</th>
+					</tr>
+					<tr>
+						<td>
+							Posádka
+						</td>
+					</tr>
+				</table>
+
+				<table class="w_img">
+				<tr>
+					<th>Místo</th>
+					<td rowspan="2" class="img" style="padding:0; position: relative;>
+						<span style="color: #fff;font-size:1px; line-height: 0.1;">.</span>
+						<?php echo do_shortcode('[gmap-embed id="97"]'); ?>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<p>A Chinese tale tells of some men sent to harm a young girl who, upon seeing her beauty, become her protectors rather than her violators. That's how I felt seeing the Earth for the first time. I could not help but love and cherish her.</p>
+					</td>
+				</tr>
+				</table>
 			</article>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
